dc6ffb018e5ea49acaea097fe1aac0a778028254
so2emma <osoemmanuel1969@gmail.com>
2024-01-25 13:54:33 +0100

    adding exceptions and http status to the question service

--- src/main/java/com/dev/quizapp/service/QuestionService.java
+++ src/main/java/com/dev/quizapp/service/QuestionService.java
@@ -1,8 +1,11 @@
 package com.dev.quizapp.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.dev.quizapp.Question;
@@ -14,16 +17,28 @@ public class QuestionService {
     @Autowired
     QuestionDoa questionDoa;
 
-    public List<Question> getAllQuestions() {
-        return questionDoa.findAll();
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        try{
+            return new ResponseEntity<>(questionDoa.findAll(), HttpStatus.OK) ;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST) ;
+
     }
 
-    public List<Question> getQuestionsByCategory(String category){
-        return questionDoa.findByCategory(category);
+    public ResponseEntity<List<Question>> getQuestionsByCategory(String category){
+        try{
+            return new ResponseEntity<>(questionDoa.findByCategory(category), HttpStatus.OK);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST) ;
     }
 
-    public String addQuestion(Question question) {
+    public ResponseEntity<String> addQuestion(Question question) {
          questionDoa.save(question);
-         return "Success";
+         return new ResponseEntity<>("Success", HttpStatus.CREATED);
     }
 }
