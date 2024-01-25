44d6b814b07ebe1eb026c476bd58731364b42139
so2emma <osoemmanuel1969@gmail.com>
2024-01-25 16:09:10 +0100

    getting using th4 questionwrapper

--- src/main/java/com/dev/quizapp/controller/QuizController.java
+++ src/main/java/com/dev/quizapp/controller/QuizController.java
@@ -1,5 +1,7 @@
 package com.dev.quizapp.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,7 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dev.quizapp.model.Question;
+import com.dev.quizapp.model.QuestionWrapper;
 import com.dev.quizapp.service.QuizService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @RequestMapping("quiz")
@@ -19,5 +26,10 @@ public class QuizController {
     @PostMapping("create")
     public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title) {
         return quizService.createQuiz(category, numQ, title);
+    }
+
+    @GetMapping("get/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id) {
+        return quizService.getQuizQuestions(id);
     }
 }
--- src/main/java/com/dev/quizapp/service/QuizService.java
+++ src/main/java/com/dev/quizapp/service/QuizService.java
@@ -1,6 +1,8 @@
 package com.dev.quizapp.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Service;
 import com.dev.quizapp.doa.QuestionDoa;
 import com.dev.quizapp.doa.QuizDoa;
 import com.dev.quizapp.model.Question;
+import com.dev.quizapp.model.QuestionWrapper;
 import com.dev.quizapp.model.Quiz;
 
 @Service
@@ -32,6 +35,19 @@ public class QuizService {
 
         return new ResponseEntity<>("Success", HttpStatus.CREATED);
 
+    }
+
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
+        Optional<Quiz> quiz = quizDoa.findById(id);
+        List<Question> questionFromDB = quiz.get().getQuestion();
+        List<QuestionWrapper> questionsForUser = new ArrayList<>();
+
+        for(Question q : questionFromDB) {
+            QuestionWrapper qw = new QuestionWrapper(q.getId(),q.getQuestionTitle(),q.getOption1(),q.getOption2(), q.getOption3(),q.getOption4());
+            questionsForUser.add(qw);
+        }
+
+        return new ResponseEntity<>(questionsForUser, HttpStatus.OK);
     }
 
 
