ecce7609e8e801ea09690ccb1a209bfffb31850f
so2emma <osoemmanuel1969@gmail.com>
2024-01-26 23:58:41 +0100

    little changes

--- src/main/java/com/dev/quizapp/controller/QuizController.java
+++ src/main/java/com/dev/quizapp/controller/QuizController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.dev.quizapp.model.Question;
+
 import com.dev.quizapp.model.QuestionWrapper;
 import com.dev.quizapp.service.QuizService;
 import org.springframework.web.bind.annotation.GetMapping;
